a5ec1c267c9ea73fd2b7de5f3bd11986508b973b
Raashid Muhammed <raashidmuhammed@feostech.com>
2024-03-30 09:47:22 +0530

    Add support to specify pins and handle errors.

--- MicrobitGPIO.py
+++ MicrobitGPIO.py
@@ -1,15 +1,26 @@
 import serial
 import uflash
 
+ANALOG_VAL_MAX = 1023
+
 class MicrobitGPIO:
-   
-    def mbit_connect(self, device, baudrate):
+
+    def __init__(self):
+        self.pin = ["pin0", "pin1", "pin2"]
+
+    def connect(self, device, baudrate):
         self.ser = serial.Serial(device, baudrate)
 
-    def mbit_runtime_flash(self):
+    def runtime_flash(self):
         uflash.flash()
 
-    def write_analog(self, val):
-        cmd = "pin1.write_analog(" + str(val) +")\r\n"
-        self.ser.write(cmd.encode("utf-8"))
-
+    def write_analog(self, pin, val):
+        try:
+            if val not in range(ANALOG_VAL_MAX):
+                raise ValueError
+            cmd = self.pin[pin] + ".write_analog(" + str(val) +")\r\n"
+            self.ser.write(cmd.encode("utf-8"))
+        except IndexError as err:
+            print("Invalid Pin number: should be 0,1,2: ", err)
+        except ValueError as err:
+            print("Invalid Analog value: should be in range 0 to 1023: ", err)
